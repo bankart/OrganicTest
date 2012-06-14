8bef66372af64f83f12b72435532947088ce79de
taehoon lee <3dasu@taehoon-ui-MacBook-Pro.local>
2012-06-14 21:16:14 +0900

    Initial Commit

---
+++ OrganicTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OrganicTest
+//
+//  Created by lee taehoon on 12. 6. 14..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
